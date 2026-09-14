fd609fb3e5bb58581d8f321d175983e05584605e
Sharon Akinlaja <msharonakinlaja@gmail.com>
2026-09-14 21:57:53 +0200

    Set up project development environment

---
+++ backend/app/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+
+from backend.app.config import settings
+
+
+database_url = URL.create(
+    drivername="postgresql+psycopg",
+    username=settings.database_user,
+    password=settings.database_password,
+    host=settings.database_host,
+    port=settings.database_port,
+    database=settings.database_name,
+)
+
+engine = create_engine(database_url)
