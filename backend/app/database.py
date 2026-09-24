12c4b2404b9be2153357c99e3042927049ab1009
Sharon Akinlaja <msharonakinlaja@gmail.com>
2026-09-24 19:13:41 +0200

    Add SQLAlchemy session factory

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
+from sqlalchemy.orm import sessionmaker
 
 from backend.app.config import settings
 
@@ -14,3 +15,4 @@ database_url = URL.create(
 )
 
 engine = create_engine(database_url)
+SessionLocal = sessionmaker(bind=engine)
